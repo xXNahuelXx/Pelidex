b61fa4f4e0c2e75da26bcc2e0c0d1e29187bce31
xXNahuelXx <nahuugh98@gmail.com>
2025-12-11 20:56:31 -0300

    Trying new methods

--- Pelidex/Pelidex/Consultas.cs
+++ Pelidex/Pelidex/Consultas.cs
@@ -10,9 +10,61 @@ namespace Pelidex
 {
     internal static class Consultas
     {
-        static Conexion conectar =new Conexion();
+        static Conexion conectar = new Conexion();
        
         public static List<Pelicula> ListarPeliculas()
+        {
+            List<Pelicula> peliculas = new List<Pelicula>();
+            List<Genero> generos = ListarGeneros();
+
+            using (SqlConnection con = conectar.AbrirConexion())
+            {
+                string consulta = "select id_Pelicula, nombre_pelicula, id_GeneroPelicula from Peliculas";
+                using (SqlCommand cmdConsulta = new SqlCommand(consulta, con))
+                {
+                    using (SqlDataReader lectura = cmdConsulta.ExecuteReader()) //Se lee lo de la tabla y lo devuelve a la variable lectura
+                    {
+                            while (lectura.Read())
+                            {
+                                foreach (Genero gen in generos)
+                                {
+                                    if (Convert.ToInt32(lectura["id_GeneroPelicula"]) == gen._idGenero)
+                                    {
+                                        peliculas.Add(new Pelicula(Convert.ToInt32(lectura["id_Pelicula"]), lectura["nombre_pelicula"].ToString(), gen._descripcion));
+                                    }
+                                }
+
+                            }
+                    }
+                }
+            }
+            return peliculas;
+        }
+
+        public static List<Genero> ListarGeneros()
+        {
+            List<Genero> generos = new List<Genero>();
+
+            using (SqlConnection con = conectar.AbrirConexion())
+            {
+                string consulta = "select id_genero, descripcion from Genero";
+                using (SqlCommand cmdConsulta = new SqlCommand(consulta, con))
+                {
+                    using (SqlDataReader lectura = cmdConsulta.ExecuteReader())
+                    {
+                        while (lectura.Read())
+                        {
+                            generos.Add(new Genero(Convert.ToInt32(lectura["id_Genero"]), lectura["descripcion"].ToString()));
+                        }
+                    }
+                }
+            }
+            return generos;
+        }
+
+
+
+        /*public static List<Pelicula> ListarPeliculas()
         {
             List<Pelicula> peliculas = new List<Pelicula>();
 
@@ -34,7 +86,7 @@ namespace Pelidex
                             peliculas.Add(new Pelicula(Convert.ToInt32(lectura["id_Pelicula"]),lectura["nombre_pelicula"].ToString(),genero._descripcion));
                         }
                     }*/
-                    string nombrePelicula = lectura["nombre_pelicula"].ToString();
+                    /*string nombrePelicula = lectura["nombre_pelicula"].ToString();
                     Console.WriteLine(nombrePelicula);
                 }
                 lectura.Close();
@@ -80,6 +132,6 @@ namespace Pelidex
                 conectar.CerrarConxion();
             }
             return generos;
-        }
+        }*/
     }
 }
